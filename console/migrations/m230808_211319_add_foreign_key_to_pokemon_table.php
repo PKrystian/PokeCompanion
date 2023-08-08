53c269665c6b817e4ba65a59a1f6a00b112cbe5d
PKrystian <krystian.pinczak2001@gmail.com>
2023-08-08 23:16:40 +0200

    dev, added 3 new migration for DB structure

---
+++ console/migrations/m230808_211319_add_foreign_key_to_pokemon_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230808_211319_add_foreign_key_to_pokemon_table
+ */
+class m230808_211319_add_foreign_key_to_pokemon_table extends Migration
+{
+    private const TABLE_NAME = 'pokemon';
+    private const FIELD_POKEMON_ID = 'pokemon_id';
+    private const FIELD_TYPE_TWO = 'type_two';
+    private const FK_TYPE_TWO = 'fk_type_two';
+    private const FIELD_EVOLVES_FROM = 'evolves_from';
+    private const FK_POKEMON_ID = 'fk_pokemon_id';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addForeignKey(
+            self::FK_TYPE_TWO,
+            self::TABLE_NAME,
+            self::FIELD_TYPE_TWO,
+            'types',
+            'type_id',
+            'SET NULL'
+        );
+        $this->addForeignKey(
+            self::FK_POKEMON_ID,
+            self::TABLE_NAME,
+            self::FIELD_EVOLVES_FROM,
+            self::TABLE_NAME,
+            self::FIELD_POKEMON_ID,
+            'SET NULL'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(self::FK_TYPE_TWO, self::TABLE_NAME);
+        $this->dropForeignKey(self::FK_POKEMON_ID, self::TABLE_NAME);
+    }
+}
