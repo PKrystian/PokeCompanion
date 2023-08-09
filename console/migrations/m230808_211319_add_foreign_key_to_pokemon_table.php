59c0bf726b42c6a20e110e4a3cdf15a62bef582e
PKrystian <krystian.pinczak2001@gmail.com>
2023-08-09 22:01:48 +0200

    dev, added constraints and foreign keys with migration to pokemon table

--- console/migrations/m230808_211319_add_foreign_key_to_pokemon_table.php
+++ console/migrations/m230808_211319_add_foreign_key_to_pokemon_table.php
@@ -9,10 +9,14 @@ class m230808_211319_add_foreign_key_to_pokemon_table extends Migration
 {
     private const TABLE_NAME = 'pokemon';
     private const FIELD_POKEMON_ID = 'pokemon_id';
+    private const FIELD_TYPE_ONE = 'type_one';
+    private const FK_TYPE_ONE = 'fk_type_one';
     private const FIELD_TYPE_TWO = 'type_two';
     private const FK_TYPE_TWO = 'fk_type_two';
     private const FIELD_EVOLVES_FROM = 'evolves_from';
     private const FK_POKEMON_ID = 'fk_pokemon_id';
+    private const FOREIGN_TABLE_NAME = 'types';
+    private const FOREIGN_FIELD_TYPE_ID = 'type_id';
 
     /**
      * {@inheritdoc}
@@ -20,11 +24,18 @@ class m230808_211319_add_foreign_key_to_pokemon_table extends Migration
     public function safeUp()
     {
         $this->addForeignKey(
+            self::FK_TYPE_ONE,
+            self::TABLE_NAME,
+            self::FIELD_TYPE_ONE,
+            self::FOREIGN_TABLE_NAME,
+            self::FOREIGN_FIELD_TYPE_ID
+        );
+        $this->addForeignKey(
             self::FK_TYPE_TWO,
             self::TABLE_NAME,
             self::FIELD_TYPE_TWO,
-            'types',
-            'type_id',
+            self::FOREIGN_TABLE_NAME,
+            self::FOREIGN_FIELD_TYPE_ID,
             'SET NULL'
         );
         $this->addForeignKey(
@@ -42,6 +53,7 @@ class m230808_211319_add_foreign_key_to_pokemon_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropForeignKey(self::FK_TYPE_ONE, self::TABLE_NAME);
         $this->dropForeignKey(self::FK_TYPE_TWO, self::TABLE_NAME);
         $this->dropForeignKey(self::FK_POKEMON_ID, self::TABLE_NAME);
     }
